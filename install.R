96bcfddc1a79475ff152a4445893c13c4a38dff6
merve-utangac <161127738+merve-utangac@users.noreply.github.com>
2024-02-27 20:55:43 +0000

    Create install.R

---
+++ install.R
@@ -0,0 +1,4 @@
+install.packages(c("tidyverse", "dplyr", "visdat", "xlsx", "Hmisc", "MASS", "car", "olsrr",
+                   "purrr", "ggcorrplot", "lavaan", "ggpubr", "performance", "ggplot2", "readxl",
+                   "corrtable", "afex", "rstatix", "scales", "apaTables", "emmeans", "report",
+                  "rempsyc"))
