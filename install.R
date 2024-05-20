e25f6ca2bdf401401fa3e3b71439a5b9378c1b65
merve-utangac <161127738+merve-utangac@users.noreply.github.com>
2024-05-20 11:35:37 +0100

    Delete install.R

--- install.R
+++
@@ -1,4 +0,0 @@
-install.packages(c("tidyverse", "dplyr", "visdat", "xlsx", "Hmisc", "MASS", "car", "olsrr",
-                   "purrr", "ggcorrplot", "lavaan", "ggpubr", "performance", "ggplot2", "readxl",
-                   "corrtable", "afex", "rstatix", "scales", "apaTables", "emmeans", "report",
-                  "rempsyc"))
